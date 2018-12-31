8f41ca9b6bf17066c83db33c89a96d59024f39eb
LanceGao <1123764761@qq.com>
2018-12-31 22:21:43 +0800

    利用缓存实现收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,20 +6,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    postDetailData: {}
+    postDetailData: {},
+    currentPostId: '',
+    isCollect: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (option) {
-    console.log(postData)
-    var postDetailData = postData.postList[option.postId]
+    var currentPostId = option.postId
+    this.data.currentPostId = currentPostId
+    var postDetailData = postData.postList[currentPostId]
     this.setData({
       postDetailData: postDetailData
     })
+
+    var postCollect = wx.getStorageSync('postCollect')
+    if (postCollect) {
+      if (postCollect[currentPostId]) {
+        this.setData({
+          isCollect: postCollect[currentPostId]
+        })
+      } 
+    } else {
+      var postCollect = {}
+      postCollect[currentPostId] = false
+      wx.setStorageSync('postCollect', postCollect)
+    }
   },
-  
+
+  onCollectionTap(event) {
+    var currentPostId = this.data.currentPostId
+    var postCollect = wx.getStorageSync('postCollect')
+    postCollect[currentPostId] = !postCollect[currentPostId]
+    wx.setStorageSync('postCollect', postCollect)
+    this.setData({
+      isCollect: postCollect[currentPostId]
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
